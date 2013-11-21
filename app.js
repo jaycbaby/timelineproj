305c4907893c492081e72afaeb7ceeb63868849e
jaycbaby <jasonchuang@Jasons-MacBook-Air-2.local>
2013-11-21 14:29:32 -0800

    11/21 2:29pm.Integrated w/ bootstrap

--- app.js
+++ app.js
@@ -1,124 +1,53 @@
-/*var express = require("express"
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-}); */
 
 
-$(document).ready(function () {
-    //JSONP timeline object will store JSON fetched from The Guardian API
-    var jsonp = {
+//make an ajax request to the guardian api
+$.ajax({
+  url: "http://content.guardianapis.com/search?q=nsa+spying+on+allies+angela&tag=tone%2Fnews%2C+type%2Farticle&section=world&from-date=2013-06-01&page-size=30&order-by=relevance&show-fields=headline%2Cstandfirst%2Cthumbnail&ids=world%2Fnsa%2C+the-nsa-files&date-id=date%2F2013&api-key=8xgymsddbs3r5tzvu2gd2kzz",
+  dataType: "jsonp"
+  //when ajax call is finished... 
+}).done(function(data){
+    //call the maketimeline function, passing in the response data object.
+  maketimeline(data);
+}); 
+
+var maketimeline = function(data) {
+
+    //timeline object will store the JSON fetched from The Guardian API
+    var articleJSON = {
         "timeline": {
-            "headline": "NSA Revelations",
-                "type": "default",
-                "text": "This is an overview of the NSA events...",
-                "startDate": " ",
-                "date": [
-                {
-                "startDate": "2012,12,12",
-                    "endDate": "",
-                    "headline": "Headline",
-                    "text": "<p>The Guardian standfirst</p>",
-                    "asset": {
-                    "media": "News thumbnail",
-                        "credit": "",
-                        "caption": ""
-                }
-            }, {
-                "startDate": "2013,1,26",
-                    "endDate": "",
-                    "headline": " ",
-                    "text": "<p>this is text.</p>",
-                    "asset": {
-                    "media": "http://youtu.be/u4XpeU9erbg",
-                        "credit": "",
-                        "caption": ""
-                }
-            }]
+            //default values
+            "headline":"NSA Revelations",
+            "type":"default",
+            "text":"<p>An overview of The Guardian's coverage of the NSA releases</p>",
+            "asset": {
+                "media":"http://gia.guim.co.uk/2013/10/nsa-decoded/html/asset/img/title-nsafiles.png",
+                "credit":"The Guardian",
+                "caption":"The Guardian"
+            },
+            //empty date array that will hold new article objects
+            "date": []
         }
     };
+
+    _.each(data.response.results, function(article, i){
+        //creating new article object and appending data into 'date' array
+        articleJSON.timeline.date.push({
+            startDate: article.webPublicationDate,
+            headline: article.fields.headline,
+            text: article.fields.standfirst,
+            asset: {
+                media: article.fields.thumbnail
+            }
+        })
+    });
 
     //TimelineJS 
     createStoryJS({
         type: 'timeline',
-        width: '800',
-        height: '600',
-        source: jsonp,
-        embed_id: 'my-timeline'
+        width: 'window.width',
+        height: '700',
+        source: articleJSON,
+        embed_id: 'myTimeline'
     });
-});
+};
 
-  	$.ajax({
-	    url: "http://content.guardianapis.com/search?q=nsa&tag=tone%2Fnews%2C+type%2Farticle&section=world&from-date=2013-06-01&page-size=30&order-by=relevance&show-fields=headline%2Cstandfirst%2Cthumbnail&ids=world%2Fthe-nsa-files&date-id=date%2F2013&api-key=8xgymsddbs3r5tzvu2gd2kzz&callback=jsoncallback",
-	    dataType: "jsonp"
-	});
-	function jsoncallback(res){
-	console.log(res);
-	}
-
-  	//populating jsonp variable
-  	function populate(){
-  	}
-
-	
-
-
-/*
-var populateTimeline = (function() {
-
-//test Youtube data
-
-	function youtube() {
-		$.get('https://gdata.youtube.com/feeds/api/users/nitesshadow/favorites?alt=json', function(data) {
-			for (var i=0; i < data.feed.entry.length; i++) {
-
-				var url = data.feed.entry[i].media$group.media$player[0].url,
-					taintedId = url.substr(url.indexOf('?v='), 14),
-					id = taintedId.replace('?v=', '');
-
-				var timestamp = Date.parse(data.feed.entry[i].published.$t),
-					date = new Date(timestamp),
-					month = date.getMonth() + 1;
-
-				var video = {
-					headline: data.feed.entry[i].title.$t,
-					text: '',
-					asset: {
-						media: 'http://youtu.be/' + id,
-						credit: data.feed.entry[i].author[0].name.$t,
-						caption: data.feed.entry[i].content.$t
-					},
-					startDate: date.getFullYear() + ',' + month + ',' + date.getDate(),
-					endDate: this.startDate
-				};
-
-				timeline.timeline.date.push(video);
-			}
-		});
-	}
-
-	return {
-		youtube: youtube
-	};
-
-}); 
-
-$.when(
-	populateTimeline.youtube()
-).then(function() {
-	createStoryJS({
-		width: window.innerWidth,
-		height: window.innerHeight,
-		source: 'js/guardiandata.jsonp',
-		embed_id: 'my-timeline'
-	});
-}
-);
-*/
